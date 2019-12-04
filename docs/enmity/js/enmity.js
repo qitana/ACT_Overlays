00eb65b3eb2a43817160b7e335b2db612e920a60
qitana <53415856+qitana@users.noreply.github.com>
2019-12-04 14:10:42 +0900

    refactoring

--- docs/enmity/js/enmity.js
+++ docs/enmity/js/enmity.js
@@ -36,23 +36,25 @@ let enmity = new Vue({
     hide: false,
     strings: {},
   },
-  attached: function() {
+  attached: function () {
     document.addEventListener('onOverlayStateUpdate', this.updateState);
     window.callOverlayHandler({ call: 'getLanguage' }).then((msg) => {
-      if (msg.language in localeStrings)
+      if (msg.language in localeStrings) {
         this.strings = localeStrings[msg.language];
-      else
+      } else {
         this.strings = localStrings['English'];
+      }
 
       window.addOverlayListener('EnmityTargetData', this.update);
       window.startOverlayEvents();
     });
   },
-  detached: function() {
+  detached: function () {
     window.removeOverlayListener('EnmityTargetData', this.update);
     document.removeEventListener('onOverlayStateUpdate', this.updateState);
   },
-  filters:{
+  filters: {
+    // Override Global 'jobrole' Filter
     jobrole: function (entity) {
       let jobName = jobEnumToName[entity.Job];
       let role = jobNameToRole[jobName];
@@ -63,7 +65,7 @@ let enmity = new Vue({
     }
   },
   methods: {
-    update: function(enmity) {
+    update: function (enmity) {
       if (enmity.Entries === null)
         enmity.Entries = [];
 
@@ -82,12 +84,13 @@ let enmity = new Vue({
           this.myEntry = e;
         }
       }
-      if (!foundMe)
+      if (!foundMe) {
         this.myEntry = noEntry;
+      }
 
-      if (enmity.Target)
+      if (enmity.Target) {
         this.processTarget(enmity.Target);
-
+      }
 
       this.updated = true;
       this.entries = enmity.Entries;
@@ -97,24 +100,24 @@ let enmity = new Vue({
       else
         document.getElementById('enmity').style.visibility = 'visible';
     },
-    updateState: function(e) {
+    updateState: function (e) {
       this.locked = e.detail.isLocked;
     },
-    toggleCollapse: function() {
+    toggleCollapse: function () {
       this.collapsed = !this.collapsed;
     },
-    toTimeString: function(time) {
+    toTimeString: function (time) {
       let totalSeconds = Math.floor(time);
       let minutes = Math.floor(totalSeconds / 60);
       let seconds = totalSeconds % 60;
       let str = '';
-      if (minutes > 0)
+      if (minutes > 0) {
         str = minutes + 'm';
-
+      }
       str += seconds + 's';
       return str;
     },
-    processTarget: function(target) {
+    processTarget: function (target) {
       target.TimeToDeath = '';
 
       // Throw away entries older than this.
@@ -136,24 +139,26 @@ let enmity = new Vue({
       if (now - h.lastUpdated > samplePeriodMs) {
         h.lastUpdated = now;
         // Don't update if hp is unchanged to keep estimate more stable.
-        if (h.hist.length == 0 || h.hist[h.hist.length - 1].hp != target.CurrentHP)
+        if (h.hist.length == 0 || h.hist[h.hist.length - 1].hp != target.CurrentHP) {
           h.hist.push({ time: now, hp: target.CurrentHP });
+        }
       }
 
-      while (h.hist.length > 0 && now - h.hist[0].time > keepHistoryMs)
+      while (h.hist.length > 0 && now - h.hist[0].time > keepHistoryMs) {
         h.hist.shift();
+      }
 
-
-      if (h.hist.length < 2)
+      if (h.hist.length < 2) {
         return;
-
+      }
 
       let first = h.hist[0];
       let last = h.hist[h.hist.length - 1];
       let totalSeconds = (last.time - first.time) / 1000;
-      if (first.hp <= last.hp || totalSeconds == 0)
-        return;
 
+      if (first.hp <= last.hp || totalSeconds == 0) {
+        return;
+      }
 
       let dps = (first.hp - last.hp) / totalSeconds;
       target.TimeToDeath = this.toTimeString(last.hp / dps);
