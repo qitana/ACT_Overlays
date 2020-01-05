65a8fd7243a7d3cd8030066a056e22a3236c8071
qitana <53415856+qitana@users.noreply.github.com>
2020-01-05 20:02:37 +0900

    support for EnmityHudList

--- docs/enmity/js/aggrolist_effects.js
+++ docs/enmity/js/aggrolist_effects.js
@@ -73,7 +73,22 @@ let vue = new Vue({
       }
 
       // Sort by aggro, descending.
-      this.combatants.sort((a, b) => b.HateRate - a.HateRate);
+      this.combatants.sort((a, b) => {
+        if (enmity.EnmityHudList) {
+          let a1 = enmity.EnmityHudList.map(x => x.ID).includes(a.ID);
+          let b1 = enmity.EnmityHudList.map(x => x.ID).includes(b.ID);
+          if (a1 && b1) {
+            let a2 = enmity.EnmityHudList.find(x => x.ID == a.ID);
+            let b2 = enmity.EnmityHudList.find(x => x.ID == b.ID);
+            return a2.Order - b2.Order;
+          } else if (a1) {
+            return -1;
+          } else if (b1) {
+            return 1;
+          }
+        }
+        return b.HateRate - a.HateRate
+      });
 
       this.updated = true;
     },
